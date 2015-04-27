8d8f1c049113d797467b49c97590299ecad8deeb
masad801 <masad.801@gmail.com>
2015-04-27 03:05:58 +0100

    Mixture Dist

--- RandomGenerationModels/main.cpp
+++ RandomGenerationModels/main.cpp
@@ -15,6 +15,7 @@ void displayUniformDistrbution(cv::Mat &inMat, int numItr = 1000000);
 void displayUniformDistrbutioni(cv::Mat &inMat, int numItr = 1000000);
 void displayGaussianDistrbutionCentralLimiti(cv::Mat &inMat, int numItr = 1000000);
 void displayGaussianDistrbutionBoxMulleri(cv::Mat &inMat, int numItr = 1000000);
+void displayMixtureDistributionBoxMulleri(cv::Mat &inMat, int numItr = 1000000);
 
 // uniform distribution generator 0 - 1 range
 double uniformDistributionRN();
@@ -22,9 +23,16 @@ double uniformDistributionRN();
 // uniform distribution generator 0 - cP range
 int uniformDistributionRNi(int cP);
 
+// gaussian distribution generation -6 - +6 range
+double centralLimit();
+double boxMuller();
+
 // gaussian distribution generation -cP - +cP range
 int gaussianDistributionCentralLimitRNi(int cP);
 int gaussianDistributionBoxMullerRNi(int cP);
+
+// mixture distribution generation -cP - +cP range
+int mixtureDistribution(int cP);
 
 int main ()
 {
@@ -42,6 +50,10 @@ int main ()
 	displayMat = cv::Mat::zeros(C_ROWS, C_COLS, CV_8UC1);
 	displayGaussianDistrbutionBoxMulleri(displayMat);
 	cv::imshow("Gaussian Distribution Box Muller", displayMat);
+
+	displayMat = cv::Mat::zeros(C_ROWS, C_COLS, CV_8UC1);
+	displayMixtureDistributionBoxMulleri(displayMat);
+	cv::imshow("Mixture Distribution Box Muller", displayMat);
 
 	cv::waitKey(0);
 
@@ -159,7 +171,32 @@ void displayGaussianDistrbutionBoxMulleri(cv::Mat &inMat, int numItr)
 	}
 }
 
+void displayMixtureDistributionBoxMulleri(cv::Mat &inMat, int numItr)
+{
+	int x, y;
 
+	int cx, cy;
+	cx = (C_COLS + 1) / 2;
+	cy = (C_ROWS + 1) / 2;
+
+	//std::cout << cx << ", " << cy << std::endl;
+
+
+	for (int i = 0; i < numItr; i++)
+	{
+
+
+		x = mixtureDistribution(cx - 1);
+		y = mixtureDistribution(cy - 1);
+
+		//std::cout << x << ", " << y << std::endl;
+
+		// display on image
+		if (inMat.at<uchar>(y + cy, x + cx) < 255)
+			inMat.at<uchar>(y + cy, x + cx) += 20;
+
+	}
+}
 
 double uniformDistributionRN()
 {
@@ -184,7 +221,8 @@ double uniformDistribution()
 
 	return retVal;
 }
-int gaussianDistributionCentralLimitRNi(int cP)
+
+double centralLimit()
 {
 	// implement central limit theorem
 	// http://en.wikipedia.org/wiki/Normal_distribution#Generating_values_from_normal_distribution
@@ -194,6 +232,12 @@ int gaussianDistributionCentralLimitRNi(int cP)
 
 	nV = nV - 6;
 
+	return nV;
+}
+int gaussianDistributionCentralLimitRNi(int cP)
+{
+	
+	double nV = centralLimit();
 	int retVal = (cP) * (nV / 6);  // normalising the distribution by 6 so the effective range is between -1 to 1 * cP
 	
 	
@@ -202,7 +246,7 @@ int gaussianDistributionCentralLimitRNi(int cP)
 	return retVal;
 }
 
-int gaussianDistributionBoxMullerRNi(int cP)
+double boxMuller()
 {
 	// implement box-muller method to convert uniform distribution to normal distribution
 	// http://en.wikipedia.org/wiki/Normal_distribution#Generating_values_from_normal_distribution
@@ -216,13 +260,19 @@ int gaussianDistributionBoxMullerRNi(int cP)
 	while (v == 0 || v == 1)
 		v = uniformDistribution();
 
-		
+
 
 	// box muller acutally generates two values X1 and X2
 	// for efficiency we can store the second value and return on second call
 	// however since that is not the objective of this tutorial therefore I am just ignoring X2
-	double X1 = sqrt(-2 * log(u)) * cos(2 * M_PI * v); 
+	double X1 = sqrt(-2 * log(u)) * cos(2 * M_PI * v);
 	//double X2 = sqrt(-2 * log(u)) * sin(2 * M_PI * v);
+
+	return X1;
+}
+int gaussianDistributionBoxMullerRNi(int cP)
+{
+	double X1 = boxMuller();
 
 	//std::cout << "u: " << u << ", v: " << v << std::endl;
 
@@ -233,4 +283,29 @@ int gaussianDistributionBoxMullerRNi(int cP)
 	int retVal = cP * X1;
 
 	return retVal;
+}
+
+int mixtureDistribution(int cP)
+{
+	startLabel:
+	// take two normal distributions and add them with weights and varying mean
+	double nD1, nD2;
+	nD1 = boxMuller();
+	nD2 = boxMuller();
+
+	// adjust the mean for both
+	nD1 = nD1 ;
+	nD2 = nD2 + 3;
+
+	// compute mixture distribution
+	double mD = (0.5*nD1 + 0.5*nD2)/6;
+	
+	if (mD > 1.0)
+		goto startLabel;
+
+	// get return value
+	int retVal = mD * cP;
+
+	return retVal;
+
 }
